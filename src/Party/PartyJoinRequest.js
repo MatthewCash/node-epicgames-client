32861bfe0139a6716b1c41a2e74cf80b9c91ac2e
matthew <matthew_cash@outlook.com>
2019-12-22 17:03:27 -0500

    Update party invites to actually work

--- src/Party/PartyJoinRequest.js
+++ src/Party/PartyJoinRequest.js
@@ -1,32 +1,43 @@
 class PartyJoinRequest {
 
-  constructor(party) {
+constructor(party) {
     this.party = party;
-  }
+}
 
-  static async send(party) {
+static async send(party) {
     const { app } = party;
     const { data } = await app.http.sendPost(
-      `https://party-service-prod.ol.epicgames.com/party/api/v1/${app.id}/parties/${party.id}/members/${app.launcher.account.id}/join`,
-      `${app.auth.tokenType} ${app.auth.accessToken}`,
-      {
+    `https://party-service-prod.ol.epicgames.com/party/api/v1/Fortnite/parties/${party.id}/members/${app.launcher.account.id}/join`,
+    `${app.auth.tokenType} ${app.auth.accessToken}`,
+    {
         connection: {
-          id: app.communicator.stream.jid.toString(),
-          meta: {
-            'urn:epic:conn:platform_s': app.config.platform.short,
-            'urn:epic:conn:type_s': 'game',
-          },
+        id: app.communicator.stream.jid.toString(),
+            meta: {
+                'urn:epic:conn:platform_s': app.config.platform.short,
+                'urn:epic:conn:type_s': 'game',
+            },
+            'yield_leadership': false,
         },
         meta: {
-          'urn:epic:member:dn_s': app.launcher.account.displayName,
-          'urn:epic:member:type_s': 'game',
-          'urn:epic:member:platform_s': app.config.platform.short,
-          'urn:epic:member:joinrequest_j': '{"CrossplayPreference_i":"1"}',
+        'urn:epic:member:dn_s': app.launcher.account.displayName,
+        'urn:epic:member:joinrequestusers_j': JSON.stringify({
+            'users': [
+                {
+                    'id': app.launcher.account.id,
+                    'dn': app.launcher.account.displayName,
+                    'plat': app.config.platform.short,
+                    'data': {
+                        'CrossplayPreference': '1',
+                        'SubGame_u': '1',
+                    }
+                }
+            ]
+        })
         },
-      },
+    },
     );
     return new this(party, data);
-  }
+}
 
 }
 
